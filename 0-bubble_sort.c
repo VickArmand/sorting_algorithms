3f3887a7235f735e76ded73ea80ee1e450fce8bf
VickArmand <victormaina1962@gmail.com>
2023-10-23 18:15:31 +0300

    0-bubble_sort.c

---
+++ 0-bubble_sort.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include "sort.h"
+/**
+ * bubble_sort - It works by comparing 2 adjacent elements
+ * starting from the left
+ * @array: integer array to be sorted
+ * @size: number of elements in array
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int temp;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
+			}
+		}
+	}
+}
---
+++ selection_sort.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "sort.h"
+
+/**
+ * selection_sort - This function sorts by
+ * obtaining minimum value of the array
+ * and placing it at the right index on the left
+ * @array: integer array
+ * @size: length of array
+ */
+void selection_sort(int *array, size_t size)
+{
+	int i, j, min, temp;
+
+	for (i = 0; i < size; i++)
+	{
+		min = array[i];
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < min)
+			{
+				min = array[j];
+				temp = array[j];
+				array[j] = array[i];
+				array[i] = temp;
+				print_array(array, size);
+			}
+		}
+	}
+}
